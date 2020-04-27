de31503d9c5e4c63f16a93615291591b04c2de68
Artem-Markula <54140481+Artem-Markula@users.noreply.github.com>
2020-04-27 19:55:04 +0300

    Create main.go

---
+++ 05. Lesson/brackets/main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+import "regexp"
+
+
+func check (f string)(w bool){
+q := ""
+s1 := 0
+s2 := 0
+s3 := 0
+
+w = false
+reg := regexp.MustCompile("[^],/[,(,),{,}]+")
+
+q = reg.ReplaceAllString(f, "")
+
+
+words := regexp.MustCompile("").Split(q, -1)
+fmt.Println(words)
+
+if len(words) == 0{
+	w = true
+    }
+ 
+for i:= 0; i<len(words); i++{
+  if words[i] == "("{
+	s1 = i + 1
+
+      if words[s1] == ")"{
+	w = true
+
+	}
+
+    }
+
+ 
+  
+  if words[i] == "["{
+	
+	s2 = i + 1
+
+      if words[s2] == "]"{
+	w = true
+
+	}
+    }
+	
+
+  if words[i] == "["{
+	
+	s2 = i + 1
+
+      if words[s2] == "]"{
+	w = true
+
+	}
+    }
+
+  if words[i] == "{"{
+	s3 = i + 1
+
+      if words[s3] == "}"{
+	w = true
+
+	}
+    }
+
+}		
+
+return w
+}
+
+
+func main(){
+
+}
